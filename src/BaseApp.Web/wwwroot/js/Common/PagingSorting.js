5c90469710a76d6fa9e1c7a6ac016410c0f3d1bb
YuriyPavlishin <yuriy.pavlishin@gmail.com>
2017-05-25 18:49:04 +0300

    Refactored pagingSorting and filter control

--- src/BaseApp.Web/wwwroot/js/Common/PagingSorting.js
+++ src/BaseApp.Web/wwwroot/js/Common/PagingSorting.js
@@ -1,154 +1,52 @@
-﻿function PagingSorting(containerSelector, urlArg, filterFormSelector) {
+﻿function PagingSorting(containerSelector, url, filterFormSelector) {
     var self = this;
-    var url;
-    var fadeDuration = 250;
-    var fadeColor = "white";
 
-    var ui = {
-        filter: {
-            container: null,
-            btnApply: null,
-            btnClear: null,
-        },
-        updateTarget: {
-            container: null,
-            getTable: null,
-            getPaging: null,
-            getState: null
-        }
+    var uiElements = {
+        container: null,
+        getTable: null,
+        getState: null
     };
+    var filter = null;
 
-    var filterHighlight = null;
-
-    function getState() {
-        var stateControl = ui.updateTarget.getState();
-        if (stateControl.length <= 0)
-            return null;
-        return JSON.parse(stateControl.attr("data-paging-sorting-state"));
+    function init() {
+        initUi();
+        subscribeToEvents();
     }
 
-    function endableTableLoading(complete) {
-        var table = ui.updateTarget.getTable();
-        if (table.length) {
-            var tableTbody = table.find("tbody");
-            var tbodyPos = tableTbody.position();
-            var divOverlay = $("<div data-loading-overlay=\"" + ui.updateTarget.container.selector + "\"></div>");
-
-            divOverlay.css({
-                position: "absolute",
-                "background-color": fadeColor,
-                display: "none",
-                top: tbodyPos.top,
-                left: tbodyPos.left,
-                width: tableTbody.width() - 1,
-                height: tableTbody.height()
-            });
-
-            tableTbody.before(divOverlay);
-            divOverlay.fadeIn({ duration: fadeDuration, complete: complete });
-        } else {
-            if (complete) {
-                complete();
-            }
-        }
-    }
-
-    function disableTableLoading(complete) {
-        var divOverlay = $("[data-loading-overlay=\"" + ui.updateTarget.container.selector + "\"]");
-        if (divOverlay.is(":visible")) {
-            divOverlay.fadeOut({
-                duration: fadeDuration,
-                complete: function () {
-                    if (complete) {
-                        complete();
-                    }
-                    divOverlay.remove();
-                }
-            });
-        } else {
-            if (complete) {
-                complete();
-            }
-        }
-    }
-
-    function ajaxRequestInternal(changedData) {
+    this.SendRequest = function (data, keepPaging) {
         var parametesData = getState();
-        var pagingSortingInfoData = parametesData["PagingSortingInfo"];
-        $.extend(pagingSortingInfoData, changedData);
-        parametesData["PagingSortingInfo"] = pagingSortingInfoData;
+        data = SerializeObject.GetObject(data);
+        if (data) {
+            $.extend(parametesData, data);
+        }
+        if (!keepPaging) {
+            $.extend(parametesData.PagingSortingInfo, { Page: 1 });
+        }
 
-        ajaxRequest(parametesData);
-    };
+        return ajaxRequest(parametesData);
+    }
 
-    function initPaging() {
-        var aPagingElements = $("a[data-Page]", ui.updateTarget.getPaging());
-        aPagingElements.unbind("click");
-        aPagingElements.click(function () {
+    function subscribeToEvents() {
+        subscribeToPaging();
+        subscribeToSorting();
+        if (filter) {
+            filter.on("filter-control-changed", function(e, args) {
+                ajaxRequest(args.filterData);
+            });
+        }
+    }
+
+    function subscribeToPaging() {
+        uiElements.container.on("click", "a[data-Page]", function () {
             var $this = $(this);
             var page = $this.attr("data-Page");
             var data = { Page: page };
-            ajaxRequestInternal(data);
+            pagingSortingChanged(data);
         });
     }
 
-    function ajaxRequest(data, callback) {
-        var $self = $(self);
-
-        if (self.ModifyRequestData != null) {
-            var modifyData = self.ModifyRequestData();
-            $.extend(data, modifyData);
-        }
-        
-        endableTableLoading(function () {
-            $.ajax({
-                url: url,
-                type: "POST",
-                data: data,
-                dataType: "text",
-                cache: false,
-                success: function (html) {
-                    ui.updateTarget.container.empty().append(html);
-                    initPaging();
-                    disableTableLoading(function () {
-
-                        var event = jQuery.Event("request-success");
-                        event.html = html;
-                        $self.trigger(event);
-
-                        if (callback) {
-                            callback();
-                        }
-                    });
-                },
-                beforeSend: function () {
-                    $self.trigger("request-start");
-                },
-                error: function (jqXHR, textStatus, errorThrown) {
-                    alert(jqXHR.responseText);
-                },
-                complete: function () {
-                    $self.trigger("request-complete");
-                    if (filterHighlight) {
-                        filterHighlight.RefreshHighlights();
-                    }
-                }
-            });
-        });
-    }
-
-    this.SendRequest = function (data, keepPaging, callback) {
-        var parametesData = getState();
-        data = SerializeObject.GetObject(data);
-        if (data) { $.extend(parametesData, data); }
-        if (!keepPaging) { $.extend(parametesData.PagingSortingInfo, { Page: 1 }); }
-        ajaxRequest(parametesData, callback);
-    };
-
-    this.ModifyRequestData = null;
-
-    function initSorting() {
-        ui.updateTarget.container.on("click", ".paging-sorting th[data-SortMember]", function () {
+    function subscribeToSorting() {
+        uiElements.container.on("click", ".paging-sorting th[data-SortMember]", function () {
             var $this = $(this);
             var sortMember = $this.attr("data-SortMember");
             var sortDescending = $this.attr("data-SortDescending");
@@ -156,90 +54,89 @@
                 SortMember: sortMember,
                 SortDescending: sortDescending
             };
-            ajaxRequestInternal(data);
+            pagingSortingChanged(data);
         });
     }
+    
+    function pagingSortingChanged(changedData) {
+        var parametesData = getState();
+        var pagingSortingInfoData = parametesData["PagingSortingInfo"];
+        $.extend(pagingSortingInfoData, changedData);
+        parametesData["PagingSortingInfo"] = pagingSortingInfoData;
 
-    function initFilterHighlight() {
-        var filterHighlightSettings = new FilterHighlightSettigs();
-        filterHighlightSettings.filterContainer = ui.filter.container;
-        filterHighlightSettings.filterBtnClear = ui.filter.btnClear;
-        filterHighlightSettings.table = ui.updateTarget.container;
-        filterHighlight = new FilterHighlight(filterHighlightSettings);
-        filterHighlight.RefreshHighlights();
+        ajaxRequest(parametesData);
+    }
+
+    function getState() {
+        var stateControl = uiElements.getState();
+        if (stateControl.length <= 0)
+            return null;
+        return JSON.parse(stateControl.attr("data-paging-sorting-state"));
+    }
+
+    function ajaxRequest(data) {
+        var $self = $(self);
+        
+        var deff = $.ajax({
+            url: url,
+            data: data,
+            cache: false,
+            type: "POST",
+            beforeSend: function () { $self.trigger("request-start"); }
+        })
+        .done(function (html) {
+            uiElements.container.empty().append(html);
+            $self.trigger("request-success");
+        })
+        .always(function () {
+            $self.trigger("request-complete");
+            refreshHighlights();
+        });
+
+        ajaxAreaLoading(uiElements.getTable().find("tbody"), deff);
+
+        return deff.promise();
     }
 
     function initUi() {
-        ui.updateTarget.container = $(".paging-sorting-update", containerSelector);
-        if (ui.updateTarget.container.length === 0) {
+        uiElements.container = $(".paging-sorting-update", containerSelector);
+        if (uiElements.container.length === 0) {
             var errMsg = ".paging-sorting-update not found in container '" + containerSelector + "' or container not found";
             alert(errMsg);
             throw new Error(errMsg);
         }
+        uiElements.getTable = function () { return $("table.paging-sorting", uiElements.container); };
+        uiElements.getState = function () { return $("[data-paging-sorting-state]", uiElements.container); };
 
+        initFilterControl();
+    }
 
-        ui.updateTarget.getTable = function () { return $("table.paging-sorting", ui.updateTarget.container); };
-        ui.updateTarget.getPaging = function () { return $("[data-id='paging']", ui.updateTarget.container); };
-        ui.updateTarget.getState = function () { return $("[data-paging-sorting-state]", ui.updateTarget.container); };
-
+    function initFilterControl() {
+        var filterContainer;
         if (filterFormSelector) {
-            ui.filter.container = $(filterFormSelector);
+            filterContainer = $(filterFormSelector);
         } else {
-            ui.filter.container = $(".paging-sorting-filter-form", containerSelector);
+            filterContainer = $(".paging-sorting-filter-form", containerSelector);
         }
-        
-        ui.filter.btnApply = $("[data-apply]", ui.filter.container);
-        ui.filter.btnClear = $("[data-clear]", ui.filter.container);
-        ui.filter.btnSave = $("[data-save-filter-type]", ui.filter.container);
-
-        if (ui.filter.btnApply.length) {
-            ui.filter.btnApply.click(function () {
-                self.SendRequest(ui.filter.container.selector);
-                return false;
-            });
-        }
-        if (ui.filter.container.length) {
-            ui.filter.container.submit(function () {
-                self.SendRequest(ui.filter.container.selector);
-                return false;
-            });
-        }
-        
-
-        if (ui.filter.btnClear.length) {
-            ui.filter.btnClear.click(function () {
-                $(":input", ui.filter.container).each(function () {
-                    var $this = $(this);
-                    $this.val(null);
-
-                    if ($this.is(".multiselect")) {
-                        $this.multiselect("refresh");
-                    }
-                    $this.removeClass("processed");
-
-                    if ($this.is("[data-jq-watermark]")) {
-                        $this.removeAttrs("data-jq-watermark");
-                        $this.watermark();
-                    }
-                });
-
-                if (ui.filter.btnClear.attr("disabled") != "disabled") {
-                    self.SendRequest(ui.filter.container.selector, null, function () {
-                    });
-                }
-                return false;
-            });
+        if (filterContainer.length > 0) {
+            filter = new appFilterControl(filterContainer);
+            refreshHighlights();
         }
     }
 
-    function init() {
-        url = urlArg;
+    function refreshHighlights() {
+        if (filter) {
+            highlightTableColumns(filter.getFilteredColumns());
+        }
+    }
 
-        initUi();
-        initSorting();
-        initPaging();
-        initFilterHighlight();
-    };
+    function highlightTableColumns(columns) {
+        $("[data-sortmember]", uiElements.container).each(function() {
+            var $this = $(this);
+            var currentColumn = $this.attr("data-sortmember");
+            $this.toggleClass("highlight", columns.indexOf(currentColumn) >= 0);
+        });
+    }
 
     init();
-};
+}
---
+++ src/BaseApp.Web/wwwroot/js/Common/ajax-area-loading.js
@@ -0,0 +1,30 @@
+﻿function ajaxAreaLoading($area, deffered) {
+    var fadeDuration = 250;
+    var fadeColor = "white";
+
+    if ($area.length <= 0)
+        return;
+
+    var tbodyPos = $area.position();
+    var divOverlay = $("<div data-loading-overlay=''></div>");
+
+    divOverlay.css({
+        position: "absolute",
+        "background-color": fadeColor,
+        display: "none",
+        top: tbodyPos.top,
+        left: tbodyPos.left,
+        width: $area.width() - 1,
+        height: $area.height()
+    });
+
+    $area.before(divOverlay);
+    divOverlay.fadeIn({ duration: fadeDuration });
+
+    deffered.always(function () {
+        divOverlay.fadeOut({
+            duration: fadeDuration,
+            complete: function () { divOverlay.remove(); }
+        });
+    });
+}
