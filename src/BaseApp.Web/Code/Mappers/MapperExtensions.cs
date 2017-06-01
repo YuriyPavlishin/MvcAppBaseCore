9fe82ca983387321f8e3444bdb9c2eb6ba8725bc
YuriyPavlishin <yuriy.pavlishin@gmail.com>
2017-06-01 19:00:21 +0300

    AutoMapper extensions fixes

--- src/BaseApp.Web/Code/Mappers/MapperExtensions.cs
+++ src/BaseApp.Web/Code/Mappers/MapperExtensions.cs
@@ -3,6 +3,7 @@ using System.Linq;
 using System.Linq.Expressions;
 using AutoMapper;
 using BaseApp.Common.Extensions;
+using BaseApp.Common.Utils;
 
 namespace BaseApp.Web.Code.Mappers
 {
@@ -22,6 +23,11 @@ namespace BaseApp.Web.Code.Mappers
 
         public static IMappingExpression<TSource, TDest> IgnoreSource<TSource, TDest>(this IMappingExpression<TSource, TDest> expression, Expression<Func<TSource, object>> property)
         {
+            var sourceName = PropertyUtil.GetName(property);
+            if (typeof(TDest).GetProperty(sourceName) != null)
+            {
+                expression.ForMember(sourceName, opt => opt.Ignore());
+            }
             expression.ForSourceMember(property, opt => opt.Ignore());
             return expression;
         }
@@ -35,10 +41,16 @@ namespace BaseApp.Web.Code.Mappers
         public static IMappingExpression<TSource, TDest> IgnoreAllUnmappedComplexTypes<TSource, TDest>(this IMappingExpression<TSource, TDest> expression)
         {
             var destType = typeof(TDest);
+            var sourceType = typeof(TSource);
+
             expression.ForAllOtherMembers(
                 delegate(IMemberConfigurationExpression<TSource, TDest, object> configurationExpression)
                 {
                     var propName = configurationExpression.DestinationMember.Name;
+                    var sourceProp = sourceType.GetProperties().Any(x => x.Name.EqualsIgnoreCase(propName));
+                    if (sourceProp)
+                        return;
+
                     var propType = destType.GetProperties().First(x => x.Name.EqualsIgnoreCase(propName)).PropertyType;
 
                     if (!propType.IsValueType
